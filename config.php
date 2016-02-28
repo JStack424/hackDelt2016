2668745b4c6e653a93098041b071ca02a68e1e23
JStack424 <JStack424@gmail.com>
2016-02-28 05:11:09 -0500

    added css file and config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+
+$servername = "localhost";
+$username = "matthewe_Matt";
+$password = "Newms5702";
+$dbname = "matthewe_politifact";
+
+    // Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
